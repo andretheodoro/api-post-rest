0897a5bce60b72d18b2bd4c2ae2c046f6a856744
tiagoesouza <tiago_e_souza@hotmail.com>
2024-09-30 23:32:29 -0300

    Adicionado swagger, typeorm e jwt no package

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,25 @@
-import express from 'express';
-import studentRouter from './routes/students/studentRoutes';  
-import teacherRouter from './routes/teachers/teacherRoutes';  
-import { dbMiddleware } from './middleware/dbMiddleware';
-import postRouter from './routes/posts/postsRoutes';
+import express from 'express'
+import studentRouter from './routes/students/studentRoutes'
+import teacherRouter from './routes/teachers/teacherRoutes'
+import postRouter from './routes/posts/postsRoutes'
+import teacherLoginRouter from './routes/teachers/teacherLoginRoutes'
+import setupSwagger from './swagger'
+import errorHandler from './middleware/errorHandler'
 
-export const app = express();
+export const app = express()
 
 // Middleware para analisar o corpo das requisições em formato JSON
-app.use(express.json());
+app.use(express.json())
 
-// Middleware para adicionar a conexão com o banco ao request
-app.use(dbMiddleware);
+// Configurar Swagger
+setupSwagger(app)
 
 // Rotas
-app.use('/api/posts', postRouter);  
-app.use('/api/posts', teacherRouter);  
-app.use('/api/posts', studentRouter);  
+app.use('/api/posts', postRouter)
+app.use('/api/posts', teacherRouter)
+app.use('/api/posts', studentRouter)
+app.use('/api', teacherLoginRouter)
 
-export default app;
+app.use(errorHandler) // Filtro de exceção deve ser o último middleware
+
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import app from './app';  // Importa o aplicativo Express configurado no app.ts
-import dotenv from 'dotenv';  // Carrega as variáveis de ambiente do arquivo .env
+import app from './app' // Importa o aplicativo Express configurado no app.ts
+import dotenv from 'dotenv' // Carrega as variáveis de ambiente do arquivo .env
 
-dotenv.config();  // Inicializa as variáveis de ambiente
+dotenv.config() // Inicializa as variáveis de ambiente
 
-const PORT = process.env.PORT || 3000;  // Define a porta para o servidor
-const environment = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 3000 // Define a porta para o servidor
+const environment = process.env.NODE_ENV || 'development'
 
 if (environment !== 'test') {
-  // Inicia o servidor e escuta as conexões
-  const server = app.listen(PORT, () => {
-    // Agora, você pode acessar o método address() no objeto server
-    const addressInfo = server.address();
-    console.log('Server listening on', addressInfo);
-  });
+    // Inicia o servidor e escuta as conexões
+    const server = app.listen(PORT, () => {
+        // Agora, você pode acessar o método address() no objeto server
+        const addressInfo = server.address()
+        console.log('Server listening on', addressInfo)
+    })
 }
